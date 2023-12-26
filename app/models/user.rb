3106001203bb8c9cafb8580d26586f36d65f3b86
FreeSoulll <greg.kr9@gmail.com>
2023-12-26 09:50:50 +0300

    feat: edits after third review

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :tests, through: :test_results
 
   def complited_tests_by_level(level)
-    Test.joins(:users).where('users.id': id, level:)
+    Test.joins(:test_results).where('test_results.user_id': id, level:)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(name: 'Eric')
-user2 = User.create(name: 'Stan')
-user3 = User.create(name: 'Kenny')
+user = User.create(
+  [
+    { name: 'Eric' },
+    { name: 'Stan' },
+    { name: 'Kenny' }
+  ]
+)
 
-category1 = Category.create(title: 'Ruby')
-category2 = Category.create(title: 'HTML')
-category3 = Category.create(title: 'Java')
+category = Category.create(
+  [
+    { title: 'Ruby' },
+    { title: 'HTML' },
+    { title: 'Java' }
+  ]
+)
 
-test1 = Test.create(title: 'Ruby test', level: 4, category_id: category1.id)
-test2 = Test.create(title: 'HTML test', level: 1, category_id: category2.id)
-test3 = Test.create(title: 'Java test', level: 3, category_id: category3.id)
+test1 = Test.create(
+  [
+    { title: 'Ruby test', level: 4, category_id: category[0].id },
+    { title: 'HTML test', level: 1, category_id: category[1].id },
+    { title: 'Java test', level: 3, category_id: category[2].id }
+  ]
+)
 
-question1 = Question.create(body: 'Who create Ruby?', tests_id: test1.id)
-question2 = Question.create(body: 'Who create HTML?', tests_id: test2.id)
-question3 = Question.create(body: 'Who create Java?', tests_id: test3.id)
+question = Question.create(
+  [
+    { body: 'Who create Ruby?', tests_id: test1[0].id },
+    { body: 'Who create HTML?', tests_id: test1[1].id },
+    { body: 'Who create Java?', tests_id: test1[2].id }
+  ]
+)
 
-answer1 = Answer.create(body: 'Yukihiro "Matz" Matsumoto', correct: true, question_id: question1.id)
-answer2 = Answer.create(body: 'Tim Berners-Lee', correct: true, question_id: question2.id)
-answer3 = Answer.create(body: 'Me', correct: false, question_id: question3.id)
+answer = Answer.create(
+  [
+    { body: 'Yukihiro "Matz" Matsumoto', correct: true, question_id: question[0].id },
+    { body: 'Tim Berners-Lee', correct: true, question_id: question[1].id },
+    { body: 'Me', correct: false, question_id: question[2].id }
+  ]
+)
 
 test_result = TestResult.new
 test_result.user = User.find(1)
