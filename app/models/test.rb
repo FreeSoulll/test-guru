ea1e774bfa6e26f9b703c55262160a7ceb894bc7
FreeSoulll <greg.kr9@gmail.com>
2023-12-27 18:44:09 +0300

    feat: add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,7 @@ class Test < ApplicationRecord
   has_many :test_results
   has_many :users, through: :test_results
   belongs_to :category
+  has_many :questions
 
   def self.sorted_tests(category)
     Test.joins(:category).order(title: :desc).where('category.title': category).pluck(:title)
