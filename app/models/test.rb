d5296dc4b561e037d9f6a74ebee813ff83977f17
FreeSoulll <greg.kr9@gmail.com>
2024-01-13 11:43:18 +0300

    feat add validations and scope

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,12 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :creator, class_name: 'User'
 
-  def self.sorted_tests(category)
-    Test.joins(:category).order(title: :desc).where('category.title': category).pluck(:title)
-  end
+  scope :easy_level, -> { where(level: 0..1) }
+  scope :medium_level, -> { where(level: 2..4) }
+  scope :hard_level, -> { where(level: 5..Float::INFINITY) }
+  scope :sorted_tests, ->(category) { Test.joins(:category).order(title: :desc).where('category.title': category).pluck(:title) }
+
+  validates :title, :level, :category_id, :creator_id, presence: true
+  validates :level, numericality: { only_integer: true, greater_than: 0 }
+  validates_uniqueness_of :title, scope: :level
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :tests, through: :test_results
   has_many :created_tests, class_name: 'Test', foreign_key: 'creator_id'
 
-  def complited_tests_by_level(level)
-    tests.where(level:)
-  end
+  scope :complited_tests_by_level, ->(level) { joins(:tests).where(tests: { level: }) }
+
+  validates :name, presence: true
 end
