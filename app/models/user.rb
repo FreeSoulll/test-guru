464d9859c595f1791a9dc2d85b59608affe6ae48
FreeSoulll <greg.kr9@gmail.com>
2024-01-14 10:44:15 +0300

    feat: asfter review

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_many :tests, through: :test_results
   has_many :created_tests, class_name: 'Test', foreign_key: 'creator_id'
 
-  scope :complited_tests_by_level, ->(level) { joins(:tests).where(tests: { level: }) }
-
   validates :name, presence: true
+
+  def complited_tests_by_level(level)
+    tests.where(level:)
+  end
 end
