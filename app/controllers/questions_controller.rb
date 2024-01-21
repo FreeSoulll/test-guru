937a31c045d54d030e9e1f3d66fad6f213bcec9d
FreeSoulll <greg.kr9@gmail.com>
2024-01-21 19:09:41 +0300

    feat: rails views

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,32 +1,43 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[new create index]
-  before_action :find_question, only: %i[show destroy]
+  before_action :find_question, only: %i[show destroy update edit]
+  before_action :find_test_from_question, only: %i[show edit destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    text = @test.questions.map { |question| "<p>#{question.body}</br></p>" }.join.html_safe
-
-    render html: text
+    @text = @test.questions.map { |question| "<p>#{question.body}</br></p>" }.join.html_safe
   end
 
   def show; end
 
-  def new; end
+  def new
+    @question = @test.questions.new
+  end
 
   def create
-    question = @test.questions.new(questions_params)
-    if question.save
-      render plain: 'Вопрос был создан'
+    @question = @test.questions.new(question_params)
+
+    if @question.save
+      redirect_to @question
     else
       render plain: 'Вопрос не  был создан'
     end
   end
 
+  def edit; end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @question.destroy
-
-    render plain: 'Вопрос был удален'
+    redirect_to test_questions_path(@test)
   end
 
   private
@@ -37,6 +48,10 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_test_from_question
+    @test = @question.test
   end
 
   def question_params
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,15 @@
 module QuestionsHelper
+  def question_header(title, type = 'update')
+    return "Create New #{title} Question" if type == 'create'
+
+    "Update New #{title} Question"
+  end
+
+  def current_year
+    Time.now.year
+  end
+
+  def github_url(author, repo)
+    "<p>Автор - #{author}</p><a href='#{repo}' target='_blank'>Test guru</a>".html_safe
+  end
 end
