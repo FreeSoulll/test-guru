3ae7f8d5b316bbacc4d994fd09ece58588804275
FreeSoulll <greg.kr9@gmail.com>
2024-03-16 16:24:19 +0300

    configure render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,7 @@
 set -o errexit
 
 bundle install
-yarn install
-NODE_OPTIONS=--openssl-legacy-provider RAILS_ENV=production bundle exec rails assets:precompile
+bundle exec rails assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
 #bundle exec rake db:seed
