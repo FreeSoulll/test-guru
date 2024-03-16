4086fafec10977f34bffdfc06508011134243e80
FreeSoulll <greg.kr9@gmail.com>
2024-03-16 13:04:08 +0300

    configure render

--- db/migrate/20231219170549_change_column_type_in_answers.rb
+++ db/migrate/20231219170549_change_column_type_in_answers.rb
@@ -1,7 +1,7 @@
 class ChangeColumnTypeInAnswers < ActiveRecord::Migration[6.1]
   def change
     Answer.where(correct: 'true').update_all(correct: true)
-    Answer.where(correct: 'false').update_all(correct: false)
+    Answer.where(correct: 'false').update_all(correct: false) 
 
     change_column :answers, :correct, :boolean, using: 'correct::boolean', default: false
   end
