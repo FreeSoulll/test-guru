ce265ba6a39b06b4f5b6dad3890d1e18f4234f15
FreeSoulll <greg.kr9@gmail.com>
2024-03-17 14:38:59 +0300

    configure render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,18 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_mailer.default_url_options = { host: 'https://test-guru-b8dg.onrender.com/admin/tests' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    addresses: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout_: 5
+  }
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
