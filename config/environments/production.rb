fe6c3b1ce52e26b932b41cdc735abbdd2a3caf89
FreeSoulll <greg.kr9@gmail.com>
2024-03-17 20:48:42 +0300

    configure render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test_guru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://test-guru-5hn2.onrender.com' }
+  config.action_mailer.default_url_options = { host: 'https://test-guru-b5gk.onrender.com' }
   config.action_mailer.default_options = { from: ENV['MAILER_FROM'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
