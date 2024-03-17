9b16b0db9b6129cd877dddae386683da53c2024f
FreeSoulll <greg.kr9@gmail.com>
2024-03-17 10:30:56 +0300

    configure render

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,11 +4,11 @@ class Answer < ApplicationRecord
   scope :corrects_answers, -> { where(correct: true) }
 
   validates :body, :question_id, presence: true
-  validate :validate_number_answers
+  validate :validate_number_answers, on: :create
 
   private
 
   def validate_number_answers
-    errors.add(:answers, 'Should have between 1-4 answers') if question&.answers&.count >= 4
+    errors.add(:answers, 'Should have between 1-4 answers') if question.answers.count >= 4
   end
 end
