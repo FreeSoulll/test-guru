4f7081f6de1a6c3f3477aa13ed5c02426513dff1
FreeSoulll <greg.kr9@gmail.com>
2024-03-18 12:31:16 +0300

    configure render

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
   root 'tests#index'
 
+  resources :feedbacks, only: %i[new create]
+
   resources :tests, only: :index do
     resources :questions, shallow: true, except: [:index] do
       resources :answers, shallow: true, except: [:index]
